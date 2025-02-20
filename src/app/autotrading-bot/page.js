d06649531d53a056856cefeed6a8bb4c3d09167d
Anubhav <xcaliber704@gmail.com>
2025-02-20 20:58:58 +0530

    video fixed

--- src/app/autotrading-bot/page.js
+++ src/app/autotrading-bot/page.js
@@ -17,7 +17,7 @@ export default function Autobot() {
                 <div className="flex flex-col md:flex-row justify-between gap-5 md:gap-10 px-5 md:px-8 py-11 items-center border border-white rounded-lg lg:border-none">
                     <h1 className="text-[#ffde71] md:hidden text-xl font-semibold">WHY ChOOSE TWJ AUTOBOT?</h1>
                     <div className=" w-full md:w-1/2 bg-[#053b2b] rounded-3xl shadow-lg">
-                        <video autoPlay loop controls className="w-full h-auto" src="/StockChartVideo.mp4"/>
+                        <video autoPlay loop controls className="w-full h-auto" src="/stockChartVideo.mp4"/>
                     </div>
                     <div className="w-full md:w-1/2 text-center md:text-left">
                         <h1 className="text-[#ffde71] text-4xl hidden md:block md:text-4xl lg:text-6xl font-bold mb-4">WHY ChOOSE TWJ AUTOBOT?</h1>
